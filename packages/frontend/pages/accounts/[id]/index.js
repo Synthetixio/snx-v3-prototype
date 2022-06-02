664545839b880b894966b45f75cd639bf244b1d8
Noah Litvin <noah.litvin@gmail.com>
2022-06-02 15:16:32 -0700

    typo fix

--- packages/frontend/pages/accounts/[id]/index.js
+++ packages/frontend/pages/accounts/[id]/index.js
@@ -2,13 +2,13 @@ import Head from 'next/head'
 import { Container, Box, Heading } from '@chakra-ui/react'
 import Subnav from '../../../components/accounts/Subnav/index'
 import Stake from '../../../components/accounts/Stake/index'
-import StakingPositions from '../../../components/accounts/StakingPositions/index'
+import StakingPositionss from '../../../components/accounts/StakingPositionss/index'
 import { useRouter } from "next/router";
 
 export default function Account() {
   const router = useRouter();
   const { id } = router.query;
-  const stakingPosition = []; //TODO: useSynthetixRead();
+  const stakingPositions = []; //TODO: useSynthetixRead();
 
   return (
     <Box>
@@ -19,8 +19,8 @@ export default function Account() {
       <Container maxW='container.sm'>
         <Box>
           <Subnav />
-          <StakingPositions stakingPosition={stakingPosition} />
-          <Heading size="md" mb="3">Stake {stakingPosition.length > 0 && 'Additional'} Collateral</Heading>
+          <StakingPositions stakingPositions={stakingPositions} />
+          <Heading size="md" mb="3">Stake {stakingPositions.length > 0 && 'Additional'} Collateral</Heading>
           <Stake />
         </Box>
       </Container>
