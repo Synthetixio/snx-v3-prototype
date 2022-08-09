11ee7fafa050cea4bfdb8b2e27d98cde818c2b9a
Noah Litvin <noah.litvin@gmail.com>
2022-08-09 15:34:28 -0400

    rewards

--- packages/frontend/components/accounts/Position/Rewards.jsx
+++ packages/frontend/components/accounts/Position/Rewards.jsx
@@ -1,22 +1,172 @@
-import { Box, Heading, Text, Flex, Button } from "@chakra-ui/react";
+import { QuestionOutlineIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Heading,
+  Text,
+  Button,
+  Flex,
+  Link,
+  Input,
+  Badge,
+  Checkbox,
+  SimpleGrid,
+  Slider,
+  SliderMark,
+  SliderTrack,
+  SliderFilledTrack,
+  Tooltip,
+  SliderThumb,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Rewards() {
+  const [sliderValue, setSliderValue] = useState(5);
+  const [showTooltip, setShowTooltip] = useState(false);
+
   return (
     <Box>
-      <Heading size="md" mb="1">
-        Earn SNX rewards by staking SNX with the Spartan Council pool
-      </Heading>
-      <Text fontSize="sm" mb="2">
-        Rewards incentivize stakers to back synthetic assets.
+      <Text mb="6">
+        In addition to fees, you’re earning eSNX rewards by backing synthetic
+        assets with SNX tokens through the Spartan Council pool.
       </Text>
-
-      <Heading size="md" mb="1">
-        Claim eSNX
-      </Heading>
-
-      <Heading size="md" mb="1">
-        Stake eSNX
-      </Heading>
+      <Box mb="10">
+        <Heading size="md" mb="1">
+          Claim eSNX
+        </Heading>
+        <Text mb="4">
+          You’re currently earning approximately 10.2 eSNX per week. You can
+          earn more eSNX by staking additional SNX or increasing your staking
+          leverage.
+        </Text>
+        <Flex alignItems="center">
+          <Button size="lg" colorScheme="green">
+            Claim 534.23 eSNX
+          </Button>
+          <Text ml="5" fontWeight="semibold" fontSize="sm">
+            <Link color="blue.400">
+              <QuestionOutlineIcon mr="1.5" transform="translateY(-1px)" />
+              How are eSNX rewards calculated?
+            </Link>
+          </Text>
+        </Flex>
+      </Box>
+      <Box mb="10">
+        <Heading size="md" mb="1">
+          Stake eSNX
+        </Heading>
+        <Text mb="4">
+          Convert eSNX to SNX by staking it. Select a longer vesting period to
+          for a better conversion rate between eSNX and SNX.
+        </Text>
+        <SimpleGrid columns={2} spacing={6}>
+          <Box>
+            <Box bg="gray.900" mb="4" p="6" pb="4" borderRadius="12px">
+              <form>
+                <Flex mb="3">
+                  <Input
+                    flex="1"
+                    type="number"
+                    border="none"
+                    placeholder="0.0"
+                    // value={null}
+                    onChange={e => {
+                      null;
+                    }}
+                  />
+                </Flex>
+              </form>
+              <Flex alignItems="center">
+                <Box>
+                  <Text fontSize="xs">Balance: 100 eSNX</Text>
+                </Box>
+                <Link>
+                  <Badge
+                    as="button"
+                    ml="3"
+                    variant="outline"
+                    colorScheme="blue"
+                    transform="translateY(-2px)"
+                  >
+                    Use Max
+                  </Badge>
+                </Link>
+              </Flex>
+            </Box>
+            <Checkbox defaultChecked>Claim and stake 534.23 eSNX</Checkbox>
+          </Box>
+          <Box>
+            <Text fontSize="sm" mb="1" fontWeight="semibold">
+              Select Vesting Period
+            </Text>
+            <Slider
+              mb="6"
+              id="slider"
+              defaultValue={5}
+              min={0}
+              max={100}
+              colorScheme="blue"
+              onChange={v => setSliderValue(v)}
+              onMouseEnter={() => setShowTooltip(true)}
+              onMouseLeave={() => setShowTooltip(false)}
+            >
+              <SliderMark
+                value={25}
+                mt="1"
+                transform="translateX(-50%)"
+                fontSize="sm"
+              >
+                25 days
+              </SliderMark>
+              <SliderMark
+                value={50}
+                mt="1"
+                transform="translateX(-50%)"
+                fontSize="sm"
+              >
+                50 days
+              </SliderMark>
+              <SliderMark
+                value={75}
+                mt="1"
+                transform="translateX(-50%)"
+                fontSize="sm"
+              >
+                75 days
+              </SliderMark>
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <Tooltip
+                hasArrow
+                bg="teal.500"
+                color="white"
+                placement="top"
+                isOpen={showTooltip}
+                label={`${sliderValue}%`}
+              >
+                <SliderThumb />
+              </Tooltip>
+            </Slider>
+            <Text fontSize="sm" mb="3">
+              600 eSNX with a 30 day vesting period will be converted to{" "}
+              <strong>400 staked SNX.</strong>
+            </Text>
+            <Button colorScheme="blue" w="100%" size="sm">
+              Stake 600 eSNX as 400 SNX
+            </Button>
+          </Box>
+        </SimpleGrid>
+      </Box>
+      <Box mb="10">
+        <Heading size="md" mb="3">
+          Vesting Schedule
+        </Heading>
+        <Box background="whiteAlpha.300" textAlign="center" p="10" mb="3">
+          Beautiful chart of vesting curve
+        </Box>
+        Your escrowed SNX cannot unstaked until it vests. Staking additional
+        eSNX will update the schedule above.
+      </Box>
     </Box>
   );
 }
